3e42a3ff041677aa4100c509784c7fff3fe5b705
Mihisara <155085512+Mihisararl@users.noreply.github.com>
2026-05-05 09:32:18 +0530

    feat: implement backend service for customer account validation and OTP management

---
+++ backend/src/main/java/com/edl/portal/model/OtpRequest.java
@@ -0,0 +1,13 @@
+package com.edl.portal.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class OtpRequest {
+    private String mobileNo;
+    private String systemCode;
+}
